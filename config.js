adf93bd3e0d8c0d0cee52baf4bd82a9c0fd9c190
YaSh18-bo <rahalhettiarachchi14@gmail.com>
2025-06-25 12:21:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -12,18 +12,18 @@ function convertToBool(text, fault = 'true') {
 
 
 const defaultConfig = {
-  SESSION_ID: "", // Put your session id here
+  SESSION_ID: "iII3BYZD#AT0kX6JDu0SBVPaDAgBl-gDpBTz4uyuGyTOzzGEP304", // Put your session id here
   ALIVE_IMG: "https://github.com/DANUWA-MD/DANUWA-BOT/blob/main/images/Danuwa%20-%20MD.png?raw=true",
-  ALIVE_MSG: "*Hello👋 DANUWA-MD Is Alive Now😍*", // Change alive msg from here
+  ALIVE_MSG: "*Hello👋 VINDI Is Alive Now😍*", // Change alive msg from here
   OPENAI_API_KEY: "",// Add OPENAI API key here
   GEMINI_API_KEY: "", // Add Gemini API key here
   REMOVE_BG_API_KEY:"", // Add removebg API key here
   WEATHER_API_KEY: "", // Add your weather API key here
-  BOT_OWNER: "94776121326", // Replace your bot owner number here with 94(country code)
-  ownerNumber: ["94776121326"], // Replace your bot owner number here (same as bot owner number)
+  BOT_OWNER: "94770693614", // Replace your bot owner number here with 94(country code)
+  ownerNumber: ["94770693614"], // Replace your bot owner number here (same as bot owner number)
   AUTO_STATUS_REACT: "true",
   AUTO_STATUS_REPLY: "true",
-  AUTO_STATUS_SEEN: "true",
+  AUTO_STATUS_SEEN: "false",
   MODE: "public", // 'private', 'public'
 };
 
